3b47a5619972e3d808df17151ea60ac0164984d6
Gino Piccirilli <gapiccirilli@outlook.com>
2023-10-03 19:25:33 -0400

    first commit

---
+++ src/main/java/com/angelopicc/flicksfeed/FlicksfeedApplication.java
@@ -0,0 +1,13 @@
+package com.angelopicc.flicksfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlicksfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlicksfeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelopicc/flicksfeed/FlicksfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelopicc.flicksfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlicksfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
